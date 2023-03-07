0c41be5892f8713c2d53370f1d229dee9b57fa9a
herennow155 <126275617+herennow155@users.noreply.github.com>
2023-03-07 22:01:39 +0545

    2 files modified, 1 added

---
+++ EditorTest.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactQuill,{Quill} from 'react-quill'
+import axios from "axios";
+import 'react-quill/dist/quill.snow.css'
+import { useMemo,useRef } from 'react';
+
+const EditorTest = () =>{
+
+
+return(
+  <div>
+    <
+    <textarea rows="10" cols ="10" />
+    <form />
+  </div>
+)
+
+}
+export default EditorTest
--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Editor from "./Editor"
+import EditorTest from "./EditorTest"
 
 import App from './App';
 
@@ -11,6 +12,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     {/* <App /> */}
-    <Editor />
+    {/* <Editor /> */}
+    <EditorTest />
   </StrictMode>
 );
